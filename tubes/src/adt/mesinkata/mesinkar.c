a471ead414ce6d6cbe069d817e00e4428c558b9b
Christopher Jie <73431564+IONISASI@users.noreply.github.com>
2021-11-11 13:59:55 +0700

    Add files via upload

---
+++ tubes/src/adt/mesinkata/mesinkar.c
@@ -0,0 +1,42 @@
+#include "mesinkar.h"
+#include <stdio.h>
+
+void logErrorThenExit(const char* error, const char* location);
+
+char CC;
+boolean EOP;
+boolean MBR;
+
+static FILE *pita;
+static int retval;
+
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   I.S. : sembarang
+   F.S. : CC adalah karakter pertama pada pita. EOP dijadikan false */
+void START()
+{
+    pita = fopen("./public/konfigurasi.txt", "r");
+    EOP = false;
+    MBR = false;
+    ADV();
+}
+
+/*  Pita dimajukan satu karakter. Jika file sudah habis, 
+    EOP akan bernilai true. MBR akan menyala true jika mesin diharapkan
+    untuk berhenti.
+    I.S. : Karakter pada jendela = CC
+    F.S. : Jika pita masih memiliki karakter, CC menjadi karakter selanjutnya
+    setelah CC yang sebelumnya*/
+void ADV()
+{
+    retval = fscanf(pita, "%c", &CC);
+    if (retval < 0)
+    {
+        if (!MBR) {
+            logErrorThenExit("Argumen terlalu sedikit", "konfigurasi.txt");
+        }
+        EOP = true;
+        fclose(pita);
+    }
+}
---
+++ tubes/src/adt/stack/stack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "stack.h"
+
+void CreateEmpty (Stack *S){
+  Top(*S) = Nil;
+};
+
+boolean IsEmpty (Stack S){
+  return Top(S) == Nil;
+};
+
+boolean IsFull (Stack S){
+  return Top(S) == MaxEl;
+};
+
+void Push (Stack * S, infotype X){
+  if (!IsEmpty(*S)) {
+    Top(*S)++;
+  } else {
+    Top(*S) = 1;
+  }
+  InfoTop(*S) = X;
+};
+
+void Pop (Stack * S, infotype* X){
+  *X = InfoTop(*S);
+  Top(*S)--;
+}
